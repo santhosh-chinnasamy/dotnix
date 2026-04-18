d7bccd30059324d1ce1b48ddc6d12307df262242
Santhosh C <csesanthosh15@gmail.com>
2026-04-18 13:03:26 +0530

    add more aliases and refactor home.nix

---
+++ users/santhosh/config/aliases.nix
@@ -0,0 +1,15 @@
+{
+  ls = "eza";
+
+  ## git
+  g = "git";
+  gst = "git status";
+  gl = "git push";
+  gp = "git push";
+  ga = "git add";
+  gc = "git commit";
+
+  # nix
+  np = "cd /etc/nixos-config/";
+  npv = "np && nvim .";
+}
--- users/santhosh/home.nix
+++ users/santhosh/home.nix
@@ -1,90 +1,39 @@
 {
   pkgs,
-  nixpkgs,
-  inputs,
   ...
 }:
 {
-  home.username = "santhosh";
-  home.homeDirectory = "/home/santhosh";
-  home.stateVersion = "25.11";
+  home = {
+    username = "santhosh";
+    homeDirectory = "/home/santhosh";
+    stateVersion = "25.11";
 
-  home.packages = with pkgs; [
-    telegram-desktop
-    ghostty
-    kitty
-    #waybar
-    #   hyprpanel
+    shellAliases = import ./config/aliases.nix;
 
-    awww
-    rofi
-    libnotify
-    brightnessctl
-    networkmanagerapplet
-  ];
-
-  programs.git = {
-    enable = true;
-    userName = "Santhosh C";
-    userEmail = "csesanthosh15@gmail.com";
+    packages = with pkgs; [
+      telegram-desktop
+      ghostty
+      kitty
+      lazygit
+      libnotify
+      brightnessctl
+      networkmanagerapplet
+    ];
   };
 
-  programs.home-manager.enable = true;
   fonts.fontconfig.enable = true;
-
-  programs.hyprpanel = {
-    enable = false;
-
-    settings = {
-      theme = "nord";
-      layout = {
-        "0" = {
-          left = [
-            "dashboard"
-            "workspaces"
-            "windowtitle"
-          ];
-          middle = [ "media" ];
-          right = [
-            "volume"
-            "network"
-            "bluetooth"
-            "systray"
-            "clock"
-            "notifications"
-          ];
-        };
-
-        "1" = {
-          left = [
-            "dashboard"
-            "workspaces"
-            "windowtitle"
-          ];
-          middle = [ "media" ];
-          right = [
-            "volume"
-            "clock"
-            "notifications"
-          ];
-        };
-
-        "2" = {
-          left = [
-            "dashboard"
-            "workspaces"
-            "windowtitle"
-          ];
-          middle = [ "media" ];
-          right = [
-            "volume"
-            "clock"
-            "notifications"
-          ];
-        };
-      };
+  programs = {
+    bash.enable = true;
+    git = {
+      enable = true;
+      userName = "Santhosh C";
+      userEmail = "csesanthosh15@gmail.com";
     };
+
+    home-manager.enable = true;
+
   };
+
   imports = [
     ./modules/hyprland.nix
     ./modules/noctalia.nix
